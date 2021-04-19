c614e6ffebc7c6a94cfc647c1c871ad6536584db
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-19 21:09:41 +0200

    Add professional seeds

--- bin/data.js
+++ bin/data.js
@@ -1,3 +1,4 @@
 module.exports.groupTypes = ["green", "orange", "red"]
 module.exports.dietTypes = ["basal", "diabetic", "glutenfree","lowsalt"]
 module.exports.genderType = ["male", "female"]
+module.exports.occupation = ["nurse","physiotherapist","doctor","director","coordinator","psychologist","occupational therapist","social worker","animator"]
--- bin/seeds.js
+++ bin/seeds.js
@@ -1,39 +1,59 @@
 const mongoose = require('mongoose')
 const faker = require('faker')
+//models
 const User = require('../models/User.model')
 const Elder = require('../models/Elder.model')
+const Professional = require('../models/Professional.model')
+
 require('../config/db.config')
-const {groupTypes, genderType, dietTypes} = require("./data")
-//Lo que queremos q haga cuando se abra la conexión de mongoose
-Promise.all([Elder.deleteMany(), User.deleteMany()]).then(() => {
-  for(let i = 0; i<50; i++){
-    User.create({
+
+const {groupTypes, genderType, dietTypes, occupation} = require("./data")
+
+Promise.all([Professional.deleteMany(), Elder.deleteMany(), User.deleteMany()]).then(() => {
+
+  for(let i = 0; i<occupation.length; i++){
+    Professional.create({
       firstname: faker.name.firstName(),
       lastname: faker.name.lastName(),
       phonenumber: faker.phone.phoneNumber(),
-      address:faker.address.direction(),
       email: faker.internet.email(),
       password: '12345678',
+      occupation: occupation[i]
     })
-    .then((u) => {
-        Elder.create({
+    .then(p => {
+      for(let i = 0; i<50; i++){
+        User.create({
           firstname: faker.name.firstName(),
           lastname: faker.name.lastName(),
-          gender: genderType[Math.floor(Math.random() * genderType.length)],
-          dateOfBirth: faker.datatype.datetime(),
-          address: faker.address.streetAddress(),
-          group: groupTypes[Math.floor(Math.random() * groupTypes.length)],
-          diet: dietTypes[Math.floor(Math.random() * dietTypes.length)],
-          user: u.id
+          phonenumber: faker.phone.phoneNumber(),
+          address:faker.address.direction(),
+          email: faker.internet.email(),
+          password: '12345678',
         })
-          .then((e) => {
-            User.findByIdAndUpdate(u.id,{elder: e.id})
-            .then(() => {
-              console.log(`Created ${e.firstname} con el familiar ${u.firstname}`)
+        .then((u) => {
+            Elder.create({
+              firstname: faker.name.firstName(),
+              lastname: faker.name.lastName(),
+              gender: genderType[Math.floor(Math.random() * genderType.length)],
+              dateOfBirth: faker.datatype.datetime(),
+              address: faker.address.streetAddress(),
+              group: groupTypes[Math.floor(Math.random() * groupTypes.length)],
+              diet: dietTypes[Math.floor(Math.random() * dietTypes.length)],
+              user: u.id
             })
+              .then((e) => {
+                User.findByIdAndUpdate(u.id,{elder: e.id})
+                .then(() => {
+                  console.log(`Created ${e.firstname} con el familiar ${u.firstname}`)
+                })
+            })
+          .catch((e) => console.log(e));
         })
-      .catch((e) => console.log(e));
+        .catch(e => console.log(e))
+        }
     })
     .catch(e => console.log(e))
-    }
+  }
 });
+
+
