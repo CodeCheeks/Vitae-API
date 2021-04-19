b8f44188c9e3fca5e6884581fcee94c2c1ec07e6
Alicia Parra <alicia@redparra.com>
2021-04-19 19:29:27 +0200

    Add elder model

---
+++ models/Elder.model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+const elderSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: 'Name is required'
+    },
+    lastname: {
+      type: String,
+      required: 'Lastname is required'
+    },
+    genre: {
+      type: String,
+      enum: ['male','female'],
+      required: 'Lastname is required'
+    },
+    dateOfBirth: {
+        type: Date,
+        required: 'Lastname is required'  
+    },
+    address: {
+      type: String,
+      required: 'Address is required'
+    },
+    group: {
+      type: String,
+      enum: ['green','orange','red'],
+    },
+    diet: {
+       type: String,
+       enum: ['basal','diabetic','glutenfree','lowsalt'],
+       default: 'basal'
+    },
+    user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = doc._id
+        delete ret._id
+        delete ret.__v
+        return ret
+      }
+    }
+  }
+);
+
+elderSchema.virtual('reports', {
+	ref: 'Report',
+	localField: 'id',
+	foreignField: 'elder'
+});
+
+elderSchema.virtual('activities', {
+	ref: 'Activity',
+	localField: 'id',
+	foreignField: 'elder'
+});
+
+
+const Elder = mongoose.model('Elder', elderSchema)
+
+module.exports = Elder
