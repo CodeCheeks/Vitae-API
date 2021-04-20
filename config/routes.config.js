aa68984744b938384c9eebb0a6c91ff961d2f2ea
Alicia Parra <alicia@redparra.com>
2021-04-20 19:33:47 +0200

    Add professionals endpoints

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller')
+const professionalsController = require('../controllers/professionals.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 
-
+//RELATIVES SECTION
 //user user
 router.post('/users', authMiddleware.isAuthenticated, usersController.create)
 router.get('/users/me', authMiddleware.isAuthenticated, usersController.get)
@@ -17,5 +18,13 @@ router.post('/login', usersController.authenticate)
 // router.delete('/users/:id', usersController.delete)
 // router.put('/users/:id', usersController.update)
 
+//PROFESSIONALS SECTION
+
+//router.post('/professionals', authMiddleware.isAuthenticated, professionalsController.create)
+router.get('/professionals', authMiddleware.isAuthenticated, professionalsController.list) 
+router.get('/professionals/me', authMiddleware.isAuthenticated, professionalsController.get)
+
+// Auth routes
+router.post('/loginProfessionals', professionalsController.authenticate)
 
 module.exports = router;
