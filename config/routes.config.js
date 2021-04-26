9b04a93a64fa7e2cc9ef445bbdc34d510c9bfdfc
Alicia Parra <alicia@redparra.com>
2021-04-26 12:58:20 +0200

    Order router

--- config/routes.config.js
+++ config/routes.config.js
@@ -8,40 +8,44 @@ const reportsController = require('../controllers/reports.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 const upload = require("./storage.config");
 
-//RELATIVES SECTION
-//user user
+//--USERS SECTION--
 router.post('/users', authMiddleware.isAuthenticated, usersController.create)
 router.get('/users/me', authMiddleware.isAuthenticated, usersController.get)
 router.get('/users', authMiddleware.isAuthenticated, usersController.list)
-
-
-// Auth routes
+// router.delete('/users/:id', usersController.delete)
+// router.put('/users/:id', usersController.update)
+// Auth 
 router.post('/login', usersController.authenticate)
 
 
-// router.delete('/users/:id', usersController.delete)
-// router.put('/users/:id', usersController.update)
-//ELDERS SECTION
+//--ELDERS SECTION--
 router.get('/elders', authMiddleware.isAuthenticated, eldersController.list)
 router.post('/addElder', authMiddleware.isAuthenticated, eldersController.addElder)
 router.put('/editElder' , authMiddleware.isAuthenticated, eldersController.editElder)
 router.delete('/deleteElder' , authMiddleware.isAuthenticated, eldersController.deleteElder)
 
-
-//REPORTS
-router.post('/reports',authMiddleware.isAuthenticated, reportsController.addReport )
-router.get('/reports',authMiddleware.isAuthenticated, reportsController.listReports )
-
-//PROFESSIONALS SECTION
+//--PROFESSIONALS SECTION--
 
 router.post('/professionals', authMiddleware.isAuthenticated, professionalsController.create)
 router.get('/professionals', authMiddleware.isAuthenticated, professionalsController.list) 
 router.get('/professionals/me', authMiddleware.isAuthenticated, professionalsController.get)
+// Auth 
+router.post('/loginProfessionals', professionalsController.authenticate)
 
+
+//--CANDIDATES SECTION--
 router.post('/employ', upload.single("cv"), candidateController.addCandidate)
 router.get('/employ', authMiddleware.isAuthenticated, candidateController.listCandidates)
 
-// Auth routes
-router.post('/loginProfessionals', professionalsController.authenticate)
+
+//--REPORTS SECTION--
+router.post('/reports',authMiddleware.isAuthenticated, reportsController.addReport )
+router.get('/reports',authMiddleware.isAuthenticated, reportsController.listReports )
+
+//--ACTIVITIES SECTION--
+
+
+//--MESSAGES SECTION--
+
 
 module.exports = router;
